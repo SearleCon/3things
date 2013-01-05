9f7332417b478f19a26486ce5b7199ca2f6e819a
Paul Janse van Rensburg <paul@searleconsulting.co.za>
2013-01-06 00:21:52 +0200

    Static pages removed action caching

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,22 +1,30 @@
 class StaticPagesController < ApplicationController
+  before_filter :generate_cache_key
 
-  #cache static pages without layout, caching is turned off by default in development mode, to turn on use  config.action_controller.perform_caching = true in development.rb under environments
-  caches_action :faq, :terms_and_conditions, :privacy_policy, :pricing, :layout => false
-
-  def about
+  private
+  def generate_cache_key
+    page_name = action_name.dup
+    @cache_key = page_name.concat("-#{AppName::REVISION}")
   end
 
-  def faq
-  end
 
-  def privacy_policy
-  end
-
-  def terms_and_conditions
-  end
-
-  def pricing
-  end
+  ##cache static pages without layout, caching is turned off by default in development mode, to turn on use  config.action_controller.perform_caching = true in development.rb under environments
+  #caches_action :faq, :terms_and_conditions, :privacy_policy, :pricing, :layout => false
+  #
+  #def about
+  #end
+  #
+  #def faq
+  #end
+  #
+  #def privacy_policy
+  #end
+  #
+  #def terms_and_conditions
+  #end
+  #
+  #def pricing
+  #end
 
 
 end
